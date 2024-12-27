9e3e50a7c38cf2e5f4d3baf20effda51854c90ad
buhariabubakar <buharikwmbo@yahoo.com>
2024-12-27 07:29:56 +0100

    updated PDF Generator

--- src/connector_pdf/commands/generatePDF.py
+++ src/connector_pdf/commands/generatePDF.py
@@ -7,12 +7,18 @@ from reportlab.lib import colors
 class PDFGenerator:
     """Class for managing and generating the final PDF."""
 
-    def __init__(self, components: list[PDFComponent], output_path: str):
-        self.components = components
+    def __init__(self, output_path: str):
         self.output_path = output_path
+        self.pdf = canvas.Canvas(self.output_path, pagesize=A4)
+
+    
+    def add_text(self, text: str, x:float, y:float, font="Helvetica", size=12):
+        self.pdf.setFont(font, size)
+        self.pdf.drawString(x, y, text)
+
 
     def generate(self):
-        pdf = canvas.Canvas(self.output_path, pagesize=A4)
-        for component in self.components:
-            component.draw(pdf)
-        pdf.save()
+        # pdf = canvas.Canvas(self.output_path, pagesize=A4)
+        # for component in self.components:
+        #     component.draw(pdf)
+        self.pdf.save()
